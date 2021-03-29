687d5ccd1b6aebcc96033aec73e2fad3c9d580cb
xonix <xonixx@gmail.com>
2021-03-29 20:39:11 +0300

    language + logo

---
+++ src/main/java/intellij_awk/SimpleIcons.java
@@ -0,0 +1,11 @@
+package intellij_awk;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class SimpleIcons {
+
+  public static final Icon FILE = IconLoader.getIcon("/icons/a.png");
+
+}
