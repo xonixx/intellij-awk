2f1317040e93353bd169c17e5d253fd686394189
xonix <xonixx@gmail.com>
2021-03-30 17:37:18 +0300

    lexer + parser

---
+++ src/main/java/intellij_awk/psi/SimpleFile.java
@@ -0,0 +1,26 @@
+package intellij_awk.psi;
+
+import com.intellij.extapi.psi.PsiFileBase;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.psi.FileViewProvider;
+import intellij_awk.SimpleFileType;
+import intellij_awk.SimpleLanguage;
+import org.jetbrains.annotations.NotNull;
+
+public class SimpleFile extends PsiFileBase {
+
+  public SimpleFile(@NotNull FileViewProvider viewProvider) {
+    super(viewProvider, SimpleLanguage.INSTANCE);
+  }
+
+  @NotNull
+  @Override
+  public FileType getFileType() {
+    return SimpleFileType.INSTANCE;
+  }
+
+  @Override
+  public String toString() {
+    return "Simple File";
+  }
+}
