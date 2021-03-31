2622c268a77944cf851dad24b5f67e3dfdee6afc
xonix <xonixx@gmail.com>
2021-04-01 01:20:17 +0300

    SimpleAnnotator

---
+++ src/main/java/intellij_awk/SimpleAnnotator.java
@@ -0,0 +1,67 @@
+package intellij_awk;
+
+import com.intellij.codeInspection.ProblemHighlightType;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLiteralExpression;
+import intellij_awk.psi.SimpleProperty;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class SimpleAnnotator implements Annotator {
+
+  // Define strings for the Simple language prefix - used for annotations, line markers, etc.
+  public static final String SIMPLE_PREFIX_STR = "simple";
+  public static final String SIMPLE_SEPARATOR_STR = ":";
+
+  @Override
+  public void annotate(@NotNull final PsiElement element, @NotNull AnnotationHolder holder) {
+    // Ensure the Psi Element is an expression
+    if (!(element instanceof PsiLiteralExpression)) {
+      return;
+    }
+
+    // Ensure the Psi element contains a string that starts with the prefix and separator
+    PsiLiteralExpression literalExpression = (PsiLiteralExpression) element;
+    String value = literalExpression.getValue() instanceof String ? (String) literalExpression.getValue() : null;
+    if ((value == null) || !value.startsWith(SIMPLE_PREFIX_STR + SIMPLE_SEPARATOR_STR)) {
+      return;
+    }
+
+    // Define the text ranges (start is inclusive, end is exclusive)
+    // "simple:key"
+    //  01234567890
+    TextRange prefixRange = TextRange.from(element.getTextRange().getStartOffset(), SIMPLE_PREFIX_STR.length() + 1);
+    TextRange separatorRange = TextRange.from(prefixRange.getEndOffset(), SIMPLE_SEPARATOR_STR.length());
+    TextRange keyRange = new TextRange(separatorRange.getEndOffset(), element.getTextRange().getEndOffset() - 1);
+
+    // highlight "simple" prefix and ":" separator
+    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+            .range(prefixRange).textAttributes(DefaultLanguageHighlighterColors.KEYWORD).create();
+    holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+            .range(separatorRange).textAttributes(SimpleSyntaxHighlighter.SEPARATOR).create();
+
+
+    // Get the list of properties for given key
+    String key = value.substring(SIMPLE_PREFIX_STR.length() + SIMPLE_SEPARATOR_STR.length());
+    List<SimpleProperty> properties = SimpleUtil.findProperties(element.getProject(), key);
+    if (properties.isEmpty()) {
+      holder.newAnnotation(HighlightSeverity.ERROR, "Unresolved property")
+              .range(keyRange)
+              .highlightType(ProblemHighlightType.LIKE_UNKNOWN_SYMBOL)
+              // ** Tutorial step 18.3 - Add a quick fix for the string containing possible properties
+              //.withFix(new SimpleCreatePropertyQuickFix(key))
+              .create();
+    } else {
+      // Found at least one property, force the text attributes to Simple syntax value character
+      holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+              .range(keyRange).textAttributes(SimpleSyntaxHighlighter.VALUE).create();
+    }
+  }
+
+}
