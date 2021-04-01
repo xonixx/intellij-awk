4a70d29711694955ad5dce347aba66b8f4d17b5b
xonix <xonixx@gmail.com>
2021-04-01 20:42:14 +0300

    code style settings

---
+++ src/main/java/intellij_awk/SimpleCodeStyleSettingsProvider.java
@@ -0,0 +1,44 @@
+package intellij_awk;
+
+import com.intellij.application.options.CodeStyleAbstractConfigurable;
+import com.intellij.application.options.CodeStyleAbstractPanel;
+import com.intellij.application.options.TabbedLanguageCodeStylePanel;
+import com.intellij.psi.codeStyle.CodeStyleConfigurable;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CodeStyleSettingsProvider;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class SimpleCodeStyleSettingsProvider extends CodeStyleSettingsProvider {
+
+  @Override
+  public CustomCodeStyleSettings createCustomSettings(CodeStyleSettings settings) {
+    return new SimpleCodeStyleSettings(settings);
+  }
+
+  @Nullable
+  @Override
+  public String getConfigurableDisplayName() {
+    return "Simple";
+  }
+
+  @NotNull
+  public CodeStyleConfigurable createConfigurable(
+      @NotNull CodeStyleSettings settings, @NotNull CodeStyleSettings modelSettings) {
+    return new CodeStyleAbstractConfigurable(
+        settings, modelSettings, this.getConfigurableDisplayName()) {
+      @Override
+      protected CodeStyleAbstractPanel createPanel(CodeStyleSettings settings) {
+        return new SimpleCodeStyleMainPanel(getCurrentSettings(), settings);
+      }
+    };
+  }
+
+  private static class SimpleCodeStyleMainPanel extends TabbedLanguageCodeStylePanel {
+
+    public SimpleCodeStyleMainPanel(CodeStyleSettings currentSettings, CodeStyleSettings settings) {
+      super(SimpleLanguage.INSTANCE, currentSettings, settings);
+    }
+  }
+}
