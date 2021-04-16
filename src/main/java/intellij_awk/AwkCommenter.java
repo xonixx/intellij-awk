fe7d388e07389f4d80ae38fc850f8e4f92e10bae
xonix <xonixx@gmail.com>
2021-04-16 21:56:04 +0300

    commenter

---
+++ src/main/java/intellij_awk/AwkCommenter.java
@@ -0,0 +1,37 @@
+package intellij_awk;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class AwkCommenter implements Commenter {
+
+  @Nullable
+  @Override
+  public String getLineCommentPrefix() {
+    return "#";
+  }
+
+  @Nullable
+  @Override
+  public String getBlockCommentPrefix() {
+    return "";
+  }
+
+  @Nullable
+  @Override
+  public String getBlockCommentSuffix() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getCommentedBlockCommentPrefix() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public String getCommentedBlockCommentSuffix() {
+    return null;
+  }
+}
