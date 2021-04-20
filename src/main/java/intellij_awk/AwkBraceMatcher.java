5f223431d83676c090a512587c679f6e6d287087
xonix <xonixx@gmail.com>
2021-04-20 18:31:12 +0300

    brace matcher

---
+++ src/main/java/intellij_awk/AwkBraceMatcher.java
@@ -0,0 +1,36 @@
+package intellij_awk;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import static intellij_awk.psi.AwkTypes.*;
+
+public class AwkBraceMatcher implements PairedBraceMatcher {
+
+  private static final BracePair[] PAIRS =
+      new BracePair[] {
+        new BracePair(LBRACE, RBRACE, false),
+        new BracePair(LBRACKET, RBRACKET, false),
+        new BracePair(LPAREN, RPAREN, false)
+      };
+
+  @Override
+  public BracePair @NotNull [] getPairs() {
+    return PAIRS;
+  }
+
+  @Override
+  public boolean isPairedBracesAllowedBeforeType(
+      @NotNull IElementType lbraceType, @Nullable IElementType contextType) {
+    return false;
+  }
+
+  @Override
+  public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+    return openingBraceOffset;
+  }
+}
