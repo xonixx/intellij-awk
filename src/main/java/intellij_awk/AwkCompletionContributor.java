90c1b4dcd867bd307c3023c471e833df01d56964
xonix <xonixx@gmail.com>
2021-04-29 20:59:17 +0300

    Completion for functions #21

---
+++ src/main/java/intellij_awk/AwkCompletionContributor.java
@@ -0,0 +1,25 @@
+package intellij_awk;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import intellij_awk.psi.SimpleTypes;
+import org.jetbrains.annotations.NotNull;
+
+public class AwkCompletionContributor extends CompletionContributor {
+
+  public AwkCompletionContributor() {
+    extend(
+        CompletionType.BASIC,
+        PlatformPatterns.psiElement(SimpleTypes.VALUE),
+        new CompletionProvider<CompletionParameters>() {
+          public void addCompletions(
+              @NotNull CompletionParameters parameters,
+              @NotNull ProcessingContext context,
+              @NotNull CompletionResultSet resultSet) {
+            resultSet.addElement(LookupElementBuilder.create("Hello"));
+          }
+        });
+  }
+}
