95a049cd346ca2ed055f185a6c66eeb67e96ffea
xonix <xonixx@gmail.com>
2021-05-01 17:31:50 +0300

    Completion for keywords #26

---
+++ src/main/java/intellij_awk/AwkCompletionContributorKeywords.java
@@ -0,0 +1,66 @@
+package intellij_awk;
+
+import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.editor.EditorModificationUtil;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.util.ProcessingContext;
+import intellij_awk.psi.AwkExpr;
+import intellij_awk.psi.impl.AwkFunctionNameImpl;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AwkCompletionContributorKeywords extends CompletionContributor {
+
+  private static final String[] builtInFunctions =
+      new String[] {
+        "atan2", "cos", "sin", "exp", "log", "sqrt", "int", "rand", "srand", "gsub", "index",
+        "length", "match", "split", "sprintf", "sub", "substr", "tolower", "toupper", "close",
+        "system"
+      };
+
+  public AwkCompletionContributorKeywords() {
+    extend(
+        CompletionType.BASIC,
+        PlatformPatterns.psiElement().inside(AwkExpr.class),
+        new CompletionProvider<>() {
+          public void addCompletions(
+              @NotNull CompletionParameters parameters,
+              @NotNull ProcessingContext context,
+              @NotNull CompletionResultSet resultSet) {
+
+            List<AwkFunctionNameImpl> functionNames =
+                AwkUtil.findFunctions(parameters.getOriginalFile());
+
+            for (String standardFunction : builtInFunctions) {
+              addFunctionCompletionCandidate(resultSet, standardFunction, true, "()");
+            }
+            for (AwkFunctionNameImpl functionName : functionNames) {
+              addFunctionCompletionCandidate(
+                  resultSet,
+                  functionName.getName(),
+                  false,
+                  "(" + String.join(", ", functionName.getArgumentNames()) + ")");
+            }
+          }
+
+          private void addFunctionCompletionCandidate(
+              @NotNull CompletionResultSet resultSet,
+              String fName,
+              boolean isBuiltIn,
+              String tailText) {
+            resultSet.addElement(
+                LookupElementBuilder.create(fName)
+                    .withTailText(tailText)
+                    .withIcon(AwkIcons.FUNCTION)
+                    .withBoldness(isBuiltIn)
+                    .withInsertHandler(
+                        (ctx, item) -> {
+                          ctx.getDocument().insertString(ctx.getSelectionEndOffset(), "()");
+                          EditorModificationUtil.moveCaretRelatively(ctx.getEditor(), 1);
+                        }));
+          }
+        });
+  }
+}
