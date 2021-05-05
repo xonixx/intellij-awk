589829e9142c4122319e307a29d8171fa77a7502
xonix <xonixx@gmail.com>
2021-05-05 18:36:35 +0300

    Add unit-test for completion #31

--- src/test/java/intellij_awk/AwkCompletionTests.java
+++ src/test/java/intellij_awk/AwkCompletionTests.java
@@ -12,27 +12,27 @@ public class AwkCompletionTests extends BasePlatformTestCase {
   public void test1() {
     checkCompletion(
         Set.of("f1", "f2", "NR", "tolower"),
-        Set.of(),
+        Set.of("BEGIN", "END"),
         "function f1() {}\nfunction f2(){}\n{ <caret> }");
   }
 
   public void test2() {
     checkCompletion(
         Set.of("f1", "f2", "NR", "tolower"),
-        Set.of(),
+        Set.of("BEGIN", "END"),
         "function f1() {}\nfunction f2(){}\n{ 1 + <caret> }");
   }
 
   public void test3() {
     checkCompletion(
         Set.of("f1", "f2", "NR", "tolower"),
-        Set.of(),
+        Set.of("BEGIN", "END"),
         "function f1() {}\nfunction f2(){}\n{ print a[<caret>] }");
   }
 
   public void test4() {
     checkCompletion(
-        Set.of("f1", "f2", "NR", "tolower"),
+        Set.of("f1", "f2", "NR", "tolower", "BEGIN", "END"),
         Set.of("return"),
         "function f1() {}\nfunction f2(){}\n<caret>");
   }
@@ -40,8 +40,15 @@ public class AwkCompletionTests extends BasePlatformTestCase {
   public void test5() {
     checkCompletion(
         Set.of("NR"),
-        Set.of("return", "f1", "f2", "tolower"),
+        Set.of("return", "f1", "f2", "tolower", "BEGIN", "END"),
         "function f1() {}\nfunction f2(){}\n{ delete <caret> }");
+  }
+
+  public void test6() {
+    checkCompletion(
+        Set.of("var1", "var2", "arg1", "arg2", "arg3", "delete", "printf"),
+        Set.of(),
+        "BEGIN { var1=1\nsplit(\"\",var2)}\nfunction f1(arg1, arg2,     arg3) { <caret> }");
   }
 
   private void checkCompletion(Set<String> required, Set<String> excluding, String code) {
