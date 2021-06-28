0ea6d22ded8d41f5416579da92ac84914c4a9551
xonix <xonixx@gmail.com>
2021-06-28 23:36:53 +0300

    Support namespaces parsing #57

---
+++ src/test/java/intellij_awk/AwkParserTestsGawk1.java
@@ -0,0 +1,27 @@
+package intellij_awk;
+
+import com.intellij.testFramework.ParsingTestCase;
+import org.jetbrains.annotations.NotNull;
+
+public class AwkParserTestsGawk1 extends ParsingTestCase {
+  public AwkParserTestsGawk1() {
+    super("parser/gawk1", "awk", new AwkParserDefinition());
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return "src/test/testData";
+  }
+
+  @Override
+  protected @NotNull String getTestName(boolean lowercaseFirstLetter) {
+    return super.getTestName(lowercaseFirstLetter).toLowerCase();
+  }
+
+  public void testNs1() { ensureOnlyParsingNoErrors(); }
+
+  private void ensureOnlyParsingNoErrors() {
+    doTest(false);
+    ensureNoErrorElements();
+  }
+}
