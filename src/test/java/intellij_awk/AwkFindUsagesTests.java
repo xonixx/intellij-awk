c5bf3ca30675f245fed0daf1251030e63b786f34
xonix <xonixx@gmail.com>
2021-07-15 02:37:56 +0300

    Imlement cross-file functions navigation #74

--- src/test/java/intellij_awk/AwkFindUsagesTests.java
+++ src/test/java/intellij_awk/AwkFindUsagesTests.java
@@ -10,15 +10,16 @@ import java.util.Collection;
 public class AwkFindUsagesTests extends BasePlatformTestCase {
 
   public void testVars1() {
-    doTest("function f(a, <caret>b, c) {\n" + "b++\n" + "print b\n" + "return f2(b)\n" + "}", 3);
+    doTest(3, "function f(a, <caret>b, c) {\n" + "b++\n" + "print b\n" + "return f2(b)\n" + "}");
   }
 
   public void testVars2() {
-    doTest("function f(a, b, c) {\n" + "<caret>b++\n" + "print b\n" + "return f2(b)\n" + "}", 3);
+    doTest(3, "function f(a, b, c) {\n" + "<caret>b++\n" + "print b\n" + "return f2(b)\n" + "}");
   }
 
   public void testVars3() {
     doTest(
+        2,
         "BEGIN {\n"
             + "}\n"
             + "\n"
@@ -26,12 +27,12 @@ public class AwkFindUsagesTests extends BasePlatformTestCase {
             + "    f1(name<caret>)\n"
             + "    name++\n"
             + "    print \"name: \" name\n"
-            + "}\n",
-        2);
+            + "}\n");
   }
 
   public void testVars4() {
     doTest(
+        3,
         "BEGIN {\n"
             + "split(\"\", name)\n"
             + "}\n"
@@ -40,12 +41,12 @@ public class AwkFindUsagesTests extends BasePlatformTestCase {
             + "    f1(<caret>name)\n"
             + "    name++\n"
             + "    print \"name: \" name\n"
-            + "}\n",
-        3);
+            + "}\n");
   }
 
   public void testVars5() {
     doTest(
+        8,
         "END {\n"
             + "split(\"\", name)\n"
             + "}\n"
@@ -56,22 +57,22 @@ public class AwkFindUsagesTests extends BasePlatformTestCase {
             + "    name++\n"
             + "    print \"name: \" name\n"
             + "}\n"
-            + "function f2() { name = toupper(name) }\n",
-        8);
+            + "function f2() { name = toupper(name) }\n");
   }
 
   public void testVars6() {
     doTest(
+        4,
         "BEGIN { <caret>a=1 }\n"
             + "a\n"
             + "{ a++ }\n"
             + "function inc() { a+=1 }\n"
-            + "END { print a }",
-        4);
+            + "END { print a }");
   }
 
   public void testFunc1() {
     doTest(
+        5,
         "BEGIN {\n"
             + "    name<caret>()\n"
             + "}\n"
@@ -84,24 +85,44 @@ public class AwkFindUsagesTests extends BasePlatformTestCase {
             + "    name()\n"
             + "    name(\"arg\")\n"
             + "    print f3(name()+1)\n"
-            + "}",
-        5);
+            + "}");
+  }
+
+  public void testFuncMultipleFiles1() {
+    doTest(
+        5,
+        "BEGIN {\n"
+            + "    name<caret>()\n"
+            + "}\n"
+            + "\n"
+            + "function name() {\n"
+            + "    name()\n"
+            + "}\n",
+        "function f2() {\n"
+            + "    name()\n"
+            + "    name(\"arg\")\n"
+            + "    print f3(name()+1)\n"
+            + "}");
   }
 
   public void testIndirect1() {
     doTest(
+        4,
         "BEGIN {\n"
             + "    a=\"fun\"\n"
             + "    @a(123)\n"
             + "    @  a<caret>()\n"
             + "}\n"
             + "a\n"
-            + "{ a++ }",
-        4);
+            + "{ a++ }");
   }
 
-  private void doTest(String code, int expectedUsagesCount) {
+  private void doTest(int expectedUsagesCount, String code, String... otherFiles) {
     myFixture.configureByText("a.awk", code);
+    for (int i = 0; i < otherFiles.length; i++) {
+      String otherFileCode = otherFiles[i];
+      myFixture.addFileToProject("f" + i + ".awk", otherFileCode);
+    }
     PsiElement element = myFixture.getElementAtCaret();
     Collection<UsageInfo> usages = myFixture.findUsages(element);
     Assert.assertEquals(expectedUsagesCount, usages.size());
