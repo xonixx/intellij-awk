014afddc789d6db866fe6cc8cc3d88e8c7093190
xonix <xonixx@gmail.com>
2021-10-09 14:00:32 +0300

    Display hint for function arguments (Ctrl-P) #32 Tweak grammar for better recoverability via pin/recoverWhile #19

---
+++ src/test/java/intellij_awk/AwkEnsurePinsTests.java
@@ -0,0 +1,31 @@
+package intellij_awk;
+
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import intellij_awk.psi.AwkFunctionName;
+
+/**
+ * These are parsing tests to ensure we still build (thanks to pins) a correct syntax tree in
+ * presence of some parsing errors.
+ */
+public class AwkEnsurePinsTests extends BasePlatformTestCase {
+
+  public void test1() {
+    checkByText("{ a(b,c,) }");
+  }
+
+  public void test2() {
+    checkByText("function f(b,c,){}");
+  }
+
+  private void checkByText(String code) {
+    String code1 = code + "\nfunction f1(){}";
+    PsiFile psiFile = myFixture.configureByText("a.awk", code1);
+    assertNotNull(
+        "element matching predicate not found, this means PSI tree is broken",
+        AwkUtil.findFirstMatchedDeep(
+            psiFile,
+            psiElement ->
+                psiElement instanceof AwkFunctionName && "f1".equals(psiElement.getText())));
+  }
+}
