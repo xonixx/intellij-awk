7c8aad2b9b1e348652b84e57e7a550c1d3b38e27
xonix <xonixx@gmail.com>
2021-10-13 13:13:02 +0300

    Release 0.2.3 #101

--- src/main/java/intellij_awk/AwkParserDefinition.java
+++ src/main/java/intellij_awk/AwkParserDefinition.java
@@ -25,7 +25,7 @@ public class AwkParserDefinition implements ParserDefinition {
   public static final IFileElementType FILE = new IStubFileElementType<>(AwkLanguage.INSTANCE) {
     @Override
     public int getStubVersion() {
-      return 9;
+      return 10;
     }
 
     @Override
