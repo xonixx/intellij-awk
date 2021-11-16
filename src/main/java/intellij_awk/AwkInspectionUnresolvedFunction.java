6eafd92a833568941930dee16fe54120d15cf84d
xonix <xonixx@gmail.com>
2021-11-16 21:09:42 +0200

    Highlight a call to unresolved function #85

--- src/main/java/intellij_awk/AwkInspectionUnresolvedFunction.java
+++ src/main/java/intellij_awk/AwkInspectionUnresolvedFunction.java
@@ -5,41 +5,40 @@ import com.intellij.codeInspection.util.IntentionFamilyName;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.search.searches.ReferencesSearch;
-import com.intellij.util.Query;
-import intellij_awk.psi.AwkFunctionNameMixin;
-import intellij_awk.psi.AwkItem;
+import com.intellij.psi.ResolveResult;
+import intellij_awk.psi.AwkFunctionCallName;
 import intellij_awk.psi.AwkVisitor;
 import org.jetbrains.annotations.NotNull;
 
 public class AwkInspectionUnresolvedFunction extends LocalInspectionTool {
 
-  private static final DeleteUnusedFunctionQuickFix deleteUnusedFunctionQuickFix =
-      new DeleteUnusedFunctionQuickFix();
-  public static final String QUICK_FIX_NAME = "Delete unused function";
+  private static final CreateNewFunctionQuickFix CREATE_NEW_FUNCTION_QUICK_FIX =
+      new CreateNewFunctionQuickFix();
+  public static final String QUICK_FIX_NAME = "Create new function with this name";
 
   @Override
   public @NotNull PsiElementVisitor buildVisitor(
       @NotNull ProblemsHolder holder, boolean isOnTheFly) {
     return new AwkVisitor() {
       @Override
-      public void visitItem(@NotNull AwkItem awkItem) {
-        AwkFunctionNameMixin functionName = (AwkFunctionNameMixin) awkItem.getFunctionName();
-        if (functionName != null) {
-          Query<PsiReference> functionReferences = ReferencesSearch.search(functionName);
-          if (!functionReferences.iterator().hasNext()) {
+      public void visitFunctionCallName(@NotNull AwkFunctionCallName functionCallName) {
+        PsiReference reference = functionCallName.getReference();
+        if (reference instanceof AwkReferenceFunction) {
+          AwkReferenceFunction referenceFunction = (AwkReferenceFunction) reference;
+          ResolveResult[] resolveResults = referenceFunction.multiResolve(false);
+          if (resolveResults.length == 0) {
             holder.registerProblem(
-                functionName,
-                "Function '" + functionName.getName() + "()' is never used",
-                ProblemHighlightType.LIKE_UNUSED_SYMBOL,
-                deleteUnusedFunctionQuickFix);
+                functionCallName,
+                "Cannot resolve function '" + functionCallName.getName() + "'",
+                ProblemHighlightType.GENERIC_ERROR,
+                CREATE_NEW_FUNCTION_QUICK_FIX);
           }
         }
       }
     };
   }
 
-  private static class DeleteUnusedFunctionQuickFix implements LocalQuickFix {
+  private static class CreateNewFunctionQuickFix implements LocalQuickFix {
 
     @Override
     public @IntentionFamilyName @NotNull String getFamilyName() {
@@ -48,7 +47,7 @@ public class AwkInspectionUnresolvedFunction extends LocalInspectionTool {
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
-      AwkUtil.findParent(descriptor.getPsiElement(), AwkItem.class).delete();
+      throw new UnsupportedOperationException("TODO");
     }
   }
 }
