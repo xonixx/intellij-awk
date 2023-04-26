d845cd798268562c14a8c414b8c0eed89678cc91
xonix <xonixx@gmail.com>
2023-04-27 02:34:03 +0300

    Repeating function param name should give an error #157

--- src/main/java/intellij_awk/AwkInspectionDuplicateFunctionParam.java
+++ src/main/java/intellij_awk/AwkInspectionDuplicateFunctionParam.java
@@ -2,8 +2,12 @@ package intellij_awk;
 
 import com.intellij.codeInspection.*;
 import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiNamedElement;
 import intellij_awk.psi.*;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
 public class AwkInspectionDuplicateFunctionParam extends LocalInspectionTool {
@@ -17,6 +21,20 @@ public class AwkInspectionDuplicateFunctionParam extends LocalInspectionTool {
         AwkParamList paramList = awkItem.getParamList();
         if (paramList != null) {
           List<AwkUserVarName> paramNameList = paramList.getUserVarNameList();
+
+          Map<String, List<AwkUserVarName>> paramsByName =
+              paramNameList.stream().collect(Collectors.groupingBy(PsiNamedElement::getName));
+
+          for (Entry<String, List<AwkUserVarName>> entry : paramsByName.entrySet()) {
+            if (entry.getValue().size() > 1) {
+              for (AwkUserVarName awkUserVarName : entry.getValue()) {
+                holder.registerProblem(
+                    awkUserVarName,
+                    "parameter '" + entry.getKey() + "' is already defined",
+                    ProblemHighlightType.GENERIC_ERROR);
+              }
+            }
+          }
         }
       }
     };
