0c2f44d3a9c309202c80d7debe5a748a33ce08ea
xonix <xonixx@gmail.com>
2023-05-10 15:29:47 +0300

    Further improvements to auto-formatter #100

--- src/main/java/intellij_awk/AwkFormattingModelBuilder.java
+++ src/main/java/intellij_awk/AwkFormattingModelBuilder.java
@@ -1,18 +1,31 @@
 package intellij_awk;
 
+import static intellij_awk.psi.AwkTypes.*;
+
 import com.intellij.formatting.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.tree.TokenSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AwkFormattingModelBuilder implements FormattingModelBuilder {
 
   private static SpacingBuilder createSpaceBuilder(CodeStyleSettings settings) {
-    return new SpacingBuilder(settings, AwkLanguage.INSTANCE);
+    return new SpacingBuilder(settings, AwkLanguage.INSTANCE)
+        .around(
+            TokenSet.create(
+                MUL, MUL_ASSIGN, ADD, ADD_ASSIGN, SUB, SUB_ASSIGN, GT, LT, GE, LE,
+                    MOD, MOD_ASSIGN,
+                    EQ, ASSIGN))
+        .spaces(1)
+        .afterInside(SEMICOLON, STATEMENT_FOR_CONDITIONS)
+        .spaces(1)
+        .beforeInside(SEMICOLON, STATEMENT_FOR_CONDITIONS)
+        .none();
   }
 
   @Override
